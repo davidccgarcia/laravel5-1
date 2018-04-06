09c61de6956bd8e46d4fd5405200b41f77c1ffb7
David García <ccristhiangarcia@gmail.com>
2018-04-06 09:56:23 -0500

    Install Bootstrap Framework and create basic login and register routes

--- config/app.php
+++ config/app.php
@@ -137,6 +137,11 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        /**
+         * Vendor Service Providers...
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -193,6 +198,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
