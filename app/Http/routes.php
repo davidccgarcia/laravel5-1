20090c48474b5818ab88f0b165a95cb79b75eec3
David García <ccristhiangarcia@gmail.com>
2018-04-06 10:20:13 -0500

    Named routes for login and register modules

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,18 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [
+    'uses' => 'HomeController@index', 
+    'as' => 'home'
+]);
 
 // Authentication routes...
-Route::get('login', 'Auth\AuthController@getLogin');
-Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('login', [
+    'uses' => 'Auth\AuthController@getLogin', 
+    'as' => 'login'
+]);
+Route::post('login', [
+    'uses' => 'Auth\AuthController@postLogin', 
+    'as' => 'login'
+]);
 Route::get('logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('register', 'Auth\AuthController@getRegister');
-Route::post('register', 'Auth\AuthController@postRegister');
+Route::get('register', [
+    'uses' => 'Auth\AuthController@getRegister', 
+    'as' => 'register'
+]);
+Route::post('register', [
+    'uses' => 'Auth\AuthController@postRegister', 
+    'as' => 'register'
+]);
 
 // Password reset link request routes...
 Route::get('password/email', 'Auth\PasswordController@getEmail');
